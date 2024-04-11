a16eb0161ea0a7410348e67b4d35b648eb152dab
Jacob Husband <jakehusband2@gmail.com>
2024-04-10 22:02:08 -0700

    Begin creating the UI

---
+++ VoltageDrop/InputControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VoltageDrop
+{
+  /// <summary>
+  /// Interaction logic for InputControl.xaml
+  /// </summary>
+  public partial class InputControl : UserControl
+  {
+    public InputControl()
+    {
+      InitializeComponent();
+    }
+  }
+}
